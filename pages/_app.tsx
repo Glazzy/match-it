200a08a69457c4f75f67463463403eb0bfe8d73b
Kevin Mangkornkaew <kevin-mangkornkaew@hotmail.com>
2022-06-19 12:19:32 +0100

    Added Google authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+  return (
+    <SessionProvider session={session} refetchInterval={0}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
-export default MyApp
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+
+export default NextAuth({
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        authorization: {
+            params: {
+              prompt: "consent",
+              access_type: "offline",
+              response_type: "code"
+            }
+          }
+      })
+    ],
+    theme: {
+      colorScheme: "light",
+    },
+    callbacks: {
+        async jwt({token, account}) {
+            if(account?.accessToken) {
+                token.accessToken = account.accessToken
+            }
+            return token
+        },
+        async session({ session, token }) {
+          // Send properties to the client, like an access_token from a provider.
+          session.accessToken = token.accessToken
+          return session
+        },
+        redirect: async ({url, baseUrl}) => {
+            console.log(baseUrl, 'test')
+            if(url === '/user') {
+                return Promise.resolve('/')
+            }
+            return Promise.resolve('/')
+        }
+    }
+  });
